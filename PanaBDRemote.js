f77f106bb313438a25adb8dd9af1104aa4d49e82
Phil Bri <ph.brioude@gmail.com>
2014-11-13 18:46:14 +0100

    socket version

--- PanaBDRemote.js
+++ PanaBDRemote.js
@@ -9,47 +9,35 @@
 */
 
 exports.action = function ( data , callback , config , SARAH ) {
-    //config = config.module.PanaBDRemote;
-    var cmd = data.cmd;
-    var blurayIP = '192.168.1.200';//config.BluRayIP;
 
-    //if ( !config.BluRayIP ) {
-    //    console.log ( 'PanaBDRemote => Pas d\'adresse IP configurée dans PanaBDRemote.prop !' );
-    //    callback ({ 'tts': 'Adresse I P incorrecte ou absente !' });
-    //    return;
-    //}
+var net = require('net');
+var opts = {
+  host: '192.168.1.200',
+  port: 80
+}
 
+var socket = net.connect(opts, function() {
+  socket.end(
+    'POST /WAN/dvdr/dvdr_ctrl.cgi HTTP/1.1\r\n' +
+    'Accept: text/html, application/xhtml+xml, */*\r\n' +
+    'Accept-Language: en-US\r\n' +
+    'Content-Type: application/x-www-form-urlencoded\r\n' +
+    'User-Agent: MEI-LAN-REMOTE-CALL\r\n' +
+    'Accept-Encoding: gzip, deflate\r\n' +
+    'Host: 192.168.1.200\r\n' +
+    'Content-Length: 39\r\n' +
+    'Connection: Keep-Alive\r\n' +
+    'Cache-Control: no-cache\r\n\r\n' +
+    'cCMD_RC_POWER.x=100&cCMD_RC_POWER.y=100\r\n');
+});
 
-    var request = require('request');
-
-    var str1 = 'cCMD_RC_' +cmd + '.x';
-    var str2 = 'cCMD_RC_' +cmd + '.y';
-    
-    var myForm = {};
-    myForm [str1]='100';
-    myForm [str2]='100';
-
-    var qs = require ('querystring');
-    var req = qs.stringify(myForm);
-
-    request ({
-        url     :   'http://192.168.1.200:80/WAN/dvdr/dvd_ctrl.cgi',
-        method  :   'post',
-        headers : {
-                    'User-Agent' : 'MEI-LAN-REMOTE-CALL',
-                    'Content-Length' : req.length,
-        },
-        form    :   myForm
-
-    }, function (err, response, body) {
-    
-        if (err || response.statusCode != 200) {
-            console.log ("BluRayRemote => L'action a échouée " + err);
-            callback ({'tts' : "L'action à échouée"});
-            return;
-        }
-        console.log ('BluRayRemote => ' + cmd + ' = OK !\r\n');
-        //console.log ('Valeur à retourner' + body + '\r\n');
+    socket.on('data', function(chunk) {
+        console.log(chunk.toString());
+        callback ({ 'tts': 'commande ok' });
     });
-    callback ({ 'tts': data.ttsAction });
+
+    socket on('error', function (e){
+        console.log('erreur = ' + e);
+    });
+
 }
